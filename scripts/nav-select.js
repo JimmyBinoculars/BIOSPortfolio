b3efe1d9e28e57c8e03769939bb0d4659a3fc6bc
JimmyBinoculars <91781252+JimmyBinoculars@users.noreply.github.com>
2025-09-15 21:12:13 +0000

    Add content selection functionality and style updates for portfolio links

--- scripts/nav-select.js
+++ scripts/nav-select.js
@@ -17,3 +17,31 @@ document.addEventListener('keydown', (e) => {
     // Add selection to the new current option
     options[currentIndex].classList.add('top-option-selected');
 });
+
+const content_options = document.querySelectorAll('.content-selectable');
+let content_currentIndex = 0;
+
+// Initialize first option as selected
+content_options[content_currentIndex].classList.add('content-selectable-selected');
+
+document.addEventListener('keydown', (e) => {
+    // Remove the selection from the current option
+    content_options[content_currentIndex].classList.remove('content-selectable-selected');
+
+    if (e.key === 'ArrowUp') {
+        content_currentIndex = (content_currentIndex + 1) % options.length;
+    } else if (e.key === 'ArrowDown') {
+        content_currentIndex = (content_currentIndex - 1 + options.length) % options.length;
+    }
+
+    // Add selection to the new current option
+    content_options[content_currentIndex].classList.add('content-selectable-selected');
+});
+
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+        if (content_currentIndex === 0) {
+            window.open('https://www.github.com/JimmyBinoculars', '_blank');
+        }
+    }
+});
